925c81fa23629a889d686a0e4d358a76d5c2462b
favz <Ogbonnafavz97@gmail.com>
2024-04-01 01:33:58 -0500

    Implemented Profile WebPage

--- src/main/java/com/favcode/favschool/model/Profile.java
+++ src/main/java/com/favcode/favschool/model/Profile.java
@@ -1,23 +1,25 @@
 package com.favcode.favschool.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
-import org.hibernate.annotations.GenericGenerator;
 
 @Data
-@Entity
-public class Address extends BaseEntity{
+public class Profile {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
-    @GenericGenerator(name = "native", strategy = "native")
-    private int addressId;
+    @NotBlank(message = "Name cannot be blank")
+    @Size(min = 3, message = "Name must be at least 3 character long")
+    private String name;
+
+    @NotBlank(message = "Mobile number cannot be blank")
+    @Pattern(regexp = "(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
+    private String mobileNumber;
+
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Please provide a valid email")
+    private String email;
 
     @NotBlank(message = "Address1 cannot be blank")
     @Size(min = 5, message = "Address1 must be at least 5 character long")
--- src/main/java/com/favcode/favschool/securrity/FavSchoolUsernamePwdAuthenticationProvider.java
+++ src/main/java/com/favcode/favschool/securrity/FavSchoolUsernamePwdAuthenticationProvider.java
@@ -32,7 +32,7 @@ public class FavSchoolUsernamePwdAuthenticationProvider implements Authenticatio
         String pwd = authentication.getCredentials().toString();
         Person person = personRepository.readByEmail(email);
         if (null != person && person.getPersonId() > 0 && passwordEncoder.matches(pwd,person.getPwd())){
-            return new UsernamePasswordAuthenticationToken(person.getName(),null,
+            return new UsernamePasswordAuthenticationToken(email,null,
                     getGrantedAuthorities(person.getRoles()));
         }else {
             throw new BadCredentialsException("Invalid Credentials");
